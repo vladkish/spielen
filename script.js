235679ff6f32dc7709d1b99efeda1d0b4a9492f5
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-06-15 17:00:33 +0100

    5 kyu 'sort array by string length

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 'use strict';
 
-// bubble sort.
 const sortByLength = function (array) {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length-1; j++) {
-      if (array[j] < array[j + 1]) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j].length > array[j + 1].length) {
         let item = array[j];
 
         array[j] = array[j + 1];
-        array[j + 1] = item
+        array[j + 1] = item;
       }
     }
   }
   return array;
-}
-console.log(sortByLength([4,45,6]));
+};
+console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"]));
