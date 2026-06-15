110c2aa0271ad421ec318dae06f1953b4f763a4d
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-06-15 16:50:42 +0100

    simple example bubble sort

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
-// Stein, Schere, Papier 
-
 'use strict';
 
-const array = [
-    'stein',
-    'schere',
-    'papier',
-]
+// bubble sort.
+const sortByLength = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length-1; j++) {
+      if (array[j] < array[j + 1]) {
+        let item = array[j];
 
-const actionComputer = array[Math.floor(Math.random() * 3)];
-const frageFurUser = confirm('Möchtest du spielen?');
-
-function aktuellFrage() {
-    switch (frageFurUser) {
-        case true:
-            let actionUser = prompt('Deine Wahl? (Stein / Schere / Papier)');
-            let count = 0;
-
-            while (!actionUser) {
-                actionUser = prompt('Deine Wahl? Zweiter Versuch...');
-                count++;
-
-                if (count == 5) {
-                    break;
-                }
-            }
-
-            return actionUser;
-        case false:
-            return 'Okay, bis bald!';
-        default:
-            return 'Vielleicht hast du ein Problem :)';
+        array[j] = array[j + 1];
+        array[j + 1] = item
+      }
     }
+  }
+  return array;
 }
-
-const AKTUELL = aktuellFrage();
-
-if (array.includes(AKTUELL.toLowerCase())) {
-    console.log(true);
-
-    if (AKTUELL == 'schere' && actionComputer == 'schere') {
-        document.write('Unentschieden!');
-    }
-
-} else {
-    console.log('Was du geschrieben hast, ist falsch. Ich möchte nicht mit dir spielen.');
-}
+console.log(sortByLength([4,45,6]));
