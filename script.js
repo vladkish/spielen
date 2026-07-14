57afed68ada866fc91bf326ae27d4be67840fb5b
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-07-14 14:44:18 +0100

    script fast way

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 'use strict';
 
+// task 1.
 const allObjectLis = document.querySelector('#categoryList');
 
 allObjectLis.addEventListener('click', (even) => {
     const item = even.target;
 
-    // reset style for every object with operation SP..READ.
+    // reset style for every object.
     [...allObjectLis.children].forEach(li => {
         li.style.cssText = '';
     });
@@ -13,14 +14,31 @@ allObjectLis.addEventListener('click', (even) => {
     const previous = item.previousElementSibling;
     const next = item.nextElementSibling;
 
+
     if (previous) {
         previous.style.cssText = `color : red`;
-        console.log(true);
     }
 
     if (next) {
         next.style.cssText = `color : red`;
-        console.log(true);
     }
     item.style.cssText = `transform : scale(1.3)`;
+
 }, {once : false});
+
+
+// task 2
+const firstButton = document.querySelector('#goFirstBtn');
+
+firstButton.addEventListener('click', () => {
+    const first = allObjectLis.firstElementChild;
+    first.style.cssText = `text-decoration : underline; color : red`;
+});
+
+
+const lastButton = document.querySelector('#goLastBtn');
+
+lastButton.addEventListener('click', () => {
+    const last = allObjectLis.lastElementChild;
+    last.style.cssText = `text-decoration : underline; color : red`;
+});
