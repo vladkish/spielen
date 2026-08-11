b4f33652c1d3a3b30d953420c8560525f8772fb2
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-08-11 17:11:13 +0200

    FAQ

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
+const faqAnswer = document.querySelectorAll('.faq-answer');
+const faqQuestion = document.querySelectorAll('.faq-question');
 
-    function hide() {
-        document.querySelectorAll('.hide').forEach(item => {
-            item.classList.remove('active');
-            item.classList.add('noactive');
+function hide() {
+    faqAnswer.forEach(item => {
+        item.style.display = 'none';
+    });
+}
+
+function show(tab=0) {
+   faqAnswer[tab].style.display = 'block'; 
+}
+hide();
+
+document.querySelector('.tab-content').addEventListener('click', (even) => {
+    const target = even.target;
+    console.log(target);
+    if (target) {
+        faqQuestion.forEach((item, index) => {
+            if (target === item) {
+                hide();
+                show(index);
+            }
         })
     }
-
-    function show(id='home') {
-        const home = document.querySelector(`#${id}`);
-
-        if (home.classList.contains('noactive')) {
-            home.classList.remove('noactive')
-        } 
-        home.classList.add('active');
-    }
-
-    // show();
-    hide();
-
-    document.body.addEventListener('click', (even) => {
-        const target = even.target;
-        if (target) {
-            document.querySelectorAll('button').forEach((item, index) => {
-                if (target === item) {
-                    hide();
-                    show(item.attributes['data-tab'].value);
-                }
-            });
-        }
-    });
-});
+})
