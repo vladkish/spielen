53b6a2ecfceffe777495f7bcb08250fab829eb9e
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-08-11 16:01:16 +0200

    advanced work with tabs

--- script.js
+++ script.js
@@ -1,125 +1,37 @@
 'use strict';
 
-// FAQ.
-function FAQ() {
-    // object answer
-    const answer = {
-        1: 'answer 1',
-        2: 'answer 2',
-        3: 'answer 3',
-        4: 'answer 4',
-    };
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabs');
+    const content = document.querySelectorAll('.content');
 
-    const blockQuestion = document.querySelector('.block-question');
-    let objectAnswer = document.createElement('h1');
+    function hiden() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        })
 
-    blockQuestion.addEventListener('click', (even) => {
-        if (even.target.matches('button') && even.target) {
-            const index = even.target.attributes['data-number'].value;
-            // create object(answer).
-            objectAnswer.textContent = answer[index];
-            even.target.append(objectAnswer);
-        }
-    });
-}
-
-// Burger menu.
-function burgerMenu() {
-    // button Menu.
-    const buttonMenu = document.querySelector('.burger');
-
-    // menuPlace.
-    const menuPlace = document.createElement('div');
-    menuPlace.style.cssText = `
-        position: absolute;
-        left: 0;
-        bottom: 0;
-
-        width: 100%;
-        height: 100%;
-        background : black;
-
-        color : white;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    `;
-
-    // all text from header.
-    const ul = document.querySelector('.ul');
-    ul.style.cssText = `
-        * {
-            color : white !important;
-        }
-    `;
-
-    // close 'X'.
-    const buttonClose = document.createElement('button');
-    buttonClose.textContent = 'X'
-    buttonClose.style.cssText = `
-        color : white;
-        background : red;
-        padding : 20px;
-        maring : 20px;
-        border : 2px solid white;
-        border-radius : 70px;
-
-        position: absolute;
-        right: 0px; 
-    `;
-
-    // function menu.
-    function menu() {
-        buttonMenu.addEventListener('click', () => {
-            if (buttonMenu.classList.contains('noactiv')) {
-                document.body.append(menuPlace);
-                menuPlace.append(ul);
-                menuPlace.append(buttonClose);
-
-                document.querySelector('.header').style.cssText = `
-                    display : none;
-                `;
-
-                buttonClose.addEventListener('click', () => {
-                    menuPlace.remove();
-
-                    document.querySelector('.header').style.cssText = `
-                        display : block;
-                    `;
-                    document.querySelector('.logo').append(ul);
-                })
-            }
-        });
+        tabs.forEach(item => {
+            item.classList.remove('active');
+        })
     }
-    menu();
-}
 
-// change theme
-function changeTheme() {
-    const buttonChange = document.querySelector('.theme-toggle');
+    function show(tab = 0) {
+        content[tab].style.display = 'block';
+        tabs[tab].classList.add('active');
+    }
 
-    const example = [
-        document.querySelector('.section-main'), 
-        document.querySelector('.header'), 
-        document.querySelector('.block')
-    ];
+    hiden();
+    show();
 
-    buttonChange.addEventListener('click', () => {
-        example.forEach(item => {
-            item.classList.toggle('dark');
-        });
+    document.body.addEventListener('click', (even) => {
+        const target = even.target;
+
+        if (target) {
+            tabs.forEach((item, index) => {
+                if (item === target) {
+                    hiden();
+                    show(index);
+                }
+            });
+        }
     });
-}
-
-// data in project.
-const data = {
-    'faq' : FAQ,
-    'changeTheme' : changeTheme,
-    'burgerMenu' : burgerMenu
-}
-
-data.faq();
-data.changeTheme();
-data.burgerMenu();
+})
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log('use script about test');
