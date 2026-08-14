2ce15e235dbab3f2746e19c8b6082e104d081b17
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-08-14 19:04:41 +0200

    I must problem and all task do

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 'use strict';
 
-const faqAnswer = document.querySelectorAll('.faq-answer');
-const faqQuestion = document.querySelectorAll('.faq-question');
+// FAQ, without DomContentLoaded if we have 'defer' in scripts(html a page).
+window.addEventListener('DOMContentLoaded', () => {
+    // main place(first section).
+    const main = document.querySelectorAll('.faq-item');
+    const question = document.querySelectorAll('.faq-question');
+    const answer = document.querySelectorAll('.faq-answer');
 
-function hide() {
-    faqAnswer.forEach(item => {
-        item.style.display = 'none';
-    });
-}
-
-function show(tab=0) {
-   faqAnswer[tab].style.display = 'block'; 
-}
-hide();
-
-document.querySelector('.tab-content').addEventListener('click', (even) => {
-    const target = even.target;
-    console.log(target);
-    if (target) {
-        faqQuestion.forEach((item, index) => {
-            if (target === item) {
-                hide();
-                show(index);
-            }
-        })
+    function hide() {
+        answer.forEach(item => {
+            item.style.display = 'none';
+        });
     }
-})
+
+    function show(tab = 1) {
+        answer[tab].style.display = 'block';
+    }
+
+    hide();
+
+    main.addEventListener('click', (even) => {
+        const target = even.target;
+        console.log(target);
+
+        if (target) {
+            question.forEach((item, index) => {
+                if (item == target) {
+                    hide();
+                    show(index);
+                }
+            })
+        }
+    })
+});
