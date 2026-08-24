8570d397baa02cb949604f1d8ba68d162fb70953
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-08-24 14:16:10 +0200

    version with forEach. but forEach work only with ArrayT

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+    // children this is HTMLCollection.
+    let section = document.querySelector('.play-section');
+    
+    const arrayButtons = {};
+
+    section.addEventListener('click', (even, index) => {
+        if (even.target.matches('.play-block')) {
+            // basic information.
+            even.preventDefault();
+            const evens = even.target;
+
+            const takeDate = evens.attributes[1].value;
+            arrayButtons[evens] = takeDate;
+
+            arrayButtons.forEach(item => {
+                item.style.background = 'red';
+            });
+        }
+    });
+})
