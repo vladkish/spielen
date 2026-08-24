e8cf9b32248ccc78dc38f547ecbfe3d7bf973eac
kishkovarv@gmail.com <kishkovarv@gmail.com>
2026-08-24 13:51:34 +0200

    did a page and style

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.button');
-
-    button.addEventListener('click', () => {
-        const script = document.createElement('script');
-        script.src = 'test.js';
-        document.body.append(script);
-    });
-});
